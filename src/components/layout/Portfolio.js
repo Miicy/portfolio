36d72594d8d529bc2020a9cd24d00938217b595e
Micy <100793297+Miicy@users.noreply.github.com>
2023-11-08 02:47:42 +0100

    fix?

--- src/components/layout/Portfolio.js
+++ src/components/layout/Portfolio.js
@@ -7,7 +7,6 @@ import itemData from "../../media/portfolio.json";
 import templateData from "../../media/templates.json";
 import ItemContainer from "../common/ItemContainer";
 
-
 function Portfolio() {
 	const [scrollPosition, setScrollPosition] = useState(0);
 	const isScreenSmall = useMediaQuery("(max-width: 400px)");
@@ -27,31 +26,43 @@ function Portfolio() {
 	useEffect(() => {
 		const scrollToShowAboutMe = 1200;
 		const aboutMeContainer = document.getElementById("portfolio-container");
-		if(isScreenSmall || isMobile || !animation) {
+		if (isScreenSmall || isMobile || !animation) {
 			aboutMeContainer.style.display = "flex";
-		}else{
-		if (aboutMeContainer) {
-			if (scrollPosition >= scrollToShowAboutMe) {
-				aboutMeContainer.style.display = "flex";
-			} else {
-				aboutMeContainer.style.display = "none";
+		} else {
+			if (aboutMeContainer) {
+				if (scrollPosition >= scrollToShowAboutMe) {
+					aboutMeContainer.style.display = "flex";
+				} else {
+					aboutMeContainer.style.display = "none";
+				}
 			}
-		}}
-	}, [scrollPosition,animation,isScreenSmall]);
-
+		}
+	}, [scrollPosition, animation, isScreenSmall]);
 
 	return (
 		<div className="portfolio" id="portfolio-container">
 			<div className="heading-smaller"> - RECENT WORK -</div>
-			<div className={itemData.items.length <= 2 ? 'inner-portfolio-less' : 'inner-portfolio'}>
+			<div
+				className={
+					itemData.items.length <= 2
+						? "inner-portfolio-less"
+						: "inner-portfolio"
+				}
+			>
 				{itemData.items.map((item, index) => (
-					<ItemContainer key={index} item={item} index={index}/>
+					<ItemContainer key={index} item={item} index={index} />
 				))}
 			</div>
 			<div className="heading-smaller"> - PAGE TEMPLATES -</div>
-			<div className={templateData.templates.length <= 2 ? 'inner-portfolio-less' : 'inner-portfolio'}>
+			<div
+				className={
+					templateData.templates.length <= 2
+						? "inner-portfolio-less"
+						: "inner-portfolio"
+				}
+			>
 				{templateData.templates.map((template, index) => (
-					<ItemContainer key={index} template={template} index={index}/>
+					<ItemContainer key={index} template={template} index={index} />
 				))}
 			</div>
 		</div>
