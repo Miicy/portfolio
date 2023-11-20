042a5cbc488b3ccb1f2ce78b8e73e2dc689bbcab
Micy <100793297+Miicy@users.noreply.github.com>
2023-11-20 23:01:28 +0100

    news link added

--- src/helpers/useGetImages.js
+++ src/helpers/useGetImages.js
@@ -1,9 +1,10 @@
 import image1 from "../media/images/webshop.png";
+import image2 from "../media/images/news.png";
 import template1 from "../media/images/rpg.png";
 import template2 from "../media/images/techwear.png";
 
 export function useGetImages(item, template, index) {
-	const portfolioImages = [null, image1, null];
+	const portfolioImages = [image2, image1, null];
 	const templateImages = [template1, template2, null];
 	if (item) {
 		return portfolioImages[index] || "none";
